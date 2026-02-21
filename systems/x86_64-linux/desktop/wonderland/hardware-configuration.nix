0053fc9e7ba98406d3a9d5d2edb09d0aa4cd6db9
Kike Fontán <26680023+CosasDePuma@users.noreply.github.com>
2026-02-21 15:36:49 +0100

    chore(repo): remove old wonderland and hacking leftovers

---
+++ systems/x86_64-linux/desktop/wonderland/hardware-configuration.nix
@@ -0,0 +1,44 @@
+{
+  config ? throw "not imported as module",
+  lib ? throw "not imported as module",
+  pkgs ? throw "not imported as module",
+  modulesPath ? throw "not imported as module",
+  ...
+}:
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "ahci"
+        "nvme"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+      ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/315513fb-f22a-415e-ac0f-026f55a3aa8d";
+      fsType = "ext4";
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/AE66-D1D8";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+  };
+
+  swapDevices = [ ];
+}
