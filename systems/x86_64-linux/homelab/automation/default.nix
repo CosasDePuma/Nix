82e3ae249f362c2e34cdc8b17f219fe80f10484e
Kike Fontán <26680023+CosasDePuma@users.noreply.github.com>
2026-04-04 05:09:47 +0200

    feat(automation): add initial homelab automation host

---
+++ systems/x86_64-linux/homelab/automation/default.nix
@@ -0,0 +1,291 @@
+{
+  config ? throw "not imported as module",
+  lib ? throw "not imported as module",
+  pkgs ? throw "not imported as module",
+  stateVersion ? "25.05",
+  ...
+}:
+{
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                   Boot                    ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  boot.loader.grub.enable = true;
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                   Disko                   ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  disko.devices = {
+    disk."disk0" = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          BOOT = {
+            size = "1M";
+            type = "EF02";
+          };
+          NIXOS = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              extraArgs = [
+                "-L"
+                "NIXOS"
+              ];
+            };
+          };
+        };
+      };
+    };
+    nodev."/tmp" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "noexec"
+        "size=1G"
+      ];
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                Environment                ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  environment.systemPackages = with pkgs; [
+    cifs-utils
+    handbrake
+  ];
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃               Filesystems                 ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  fileSystems = {
+    "/mnt/backups" = {
+      device = "//192.168.1.3/backups";
+      fsType = "cifs";
+      options = [
+        "credentials=${config.age.secrets."smb.creds".path}"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.mount-timeout=5s"
+      ];
+    };
+    "/mnt/media" = {
+      device = "//192.168.1.3/media";
+      fsType = "cifs";
+      options = [
+        "credentials=${config.age.secrets."smb.creds".path}"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.device-timeout=5s"
+        "x-systemd.idle-timeout=60"
+        "x-systemd.mount-timeout=5s"
+      ];
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                 Hardware                  ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  hardware.enableAllHardware = true;
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                Inputs: Age                ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  age = {
+    identityPaths = builtins.map (key: key.path) config.services.openssh.hostKeys;
+    secrets."smb.creds".file = ./.smb/smb.creds.age;
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                Networking                 ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  networking = {
+    # --- host
+    hostName = "automation";
+    hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
+    # --- interfaces
+    usePredictableInterfaceNames = false;
+    interfaces = {
+      # --- physical interfaces
+      "eth0" = {
+        useDHCP = false;
+        ipv4.addresses = [
+          {
+            address = "10.0.10.4";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+    # --- gateway
+    defaultGateway = {
+      interface = "eth0";
+      address = "10.0.10.254";
+    };
+    # --- dns
+    domain = "home";
+    search = [ "home" ];
+    nameservers = [ "10.0.10.254" ];
+    # --- firewall
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        5678 # n8n
+        64022 # SSH
+      ];
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                  Nixpkgs                  ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  nixpkgs.config.allowUnfree = true;
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                  Security                 ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  security = {
+    pam = {
+      sshAgentAuth.enable = true;
+      services."sudo".sshAgentAuth = true;
+    };
+    sudo-rs = {
+      enable = true;
+      execWheelOnly = true;
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                 Services                  ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  services = {
+
+    # ┌──────────────────────────────────────┐
+    # |                 n8n                  │
+    # └──────────────────────────────────────┘
+
+    n8n = {
+      enable = true;
+      environment.WEBHOOK_URL = "https://automate.kike.wtf";
+    };
+
+    # ┌──────────────────────────────────────┐
+    # │                OpenSSH               │
+    # └──────────────────────────────────────┘
+
+    openssh = {
+      enable = true;
+      allowSFTP = true;
+      authorizedKeysInHomedir = false;
+      banner = builtins.readFile ./.ssh/banner.txt;
+      listenAddresses = [
+        {
+          addr = "0.0.0.0";
+          port = 64022;
+        }
+      ];
+      ports = [ ];
+      startWhenNeeded = true;
+      settings = {
+        AllowUsers = lib.attrsets.mapAttrsToList (name: _: name) (
+          lib.attrsets.filterAttrs (_: v: builtins.elem "sshuser" v.extraGroups) config.users.users
+        );
+        AuthorizedPrincipalsFile = "none";
+        ChallengeResponseAuthentication = false;
+        Ciphers = [
+          "chacha20-poly1305@openssh.com"
+          "aes256-gcm@openssh.com"
+          "aes128-gcm@openssh.com"
+          "aes256-ctr"
+          "aes192-ctr"
+          "aes128-ctr"
+        ];
+        ClientAliveInterval = 300;
+        GatewayPorts = "no";
+        IgnoreRhosts = true;
+        KbdInteractiveAuthentication = false;
+        KexAlgorithms = [
+          "curve25519-sha256@libssh.org"
+          "diffie-hellman-group16-sha512"
+          "diffie-hellman-group18-sha512"
+        ];
+        LogLevel = "VERBOSE";
+        LoginGraceTime = "30";
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+        ];
+        MaxAuthTries = 3;
+        MaxSessions = 5;
+        MaxStartups = "10:30:100";
+        PasswordAuthentication = false;
+        PermitEmptyPasswords = false;
+        PermitRootLogin = "no";
+        PrintMotd = false;
+        StrictModes = true;
+        UseDns = false;
+        UsePAM = true;
+        X11Forwarding = false;
+      };
+    };
+
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                  System                   ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  system = {
+    inherit stateVersion;
+    autoUpgrade = {
+      enable = true;
+      flake = "github:cosasdepuma/nix";
+      dates = "daily";
+      operation = "switch";
+      persistent = true;
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                   Users                   ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  users = {
+    mutableUsers = false;
+    groups = {
+      sshuser = { };
+      users = { };
+    };
+    users.automation = {
+      isNormalUser = true;
+      description = "Automation management user";
+      initialPassword = null;
+      home = "/home/users/automation";
+      uid = 1000;
+      group = "users";
+      useDefaultShell = true;
+      extraGroups = [
+        "wheel"
+        "sshuser"
+      ];
+      openssh.authorizedKeys.keys = lib.strings.splitString "\n" (
+        builtins.readFile ./.ssh/authorized_keys
+      );
+    };
+  };
+}
