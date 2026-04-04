56ceb76273a3dbc99de7cd372a745e1ec94a7d3f
Kike Fontán <26680023+CosasDePuma@users.noreply.github.com>
2026-04-04 05:09:27 +0200

    feat(proxy): add initial homelab proxy host with caddy and homepage

---
+++ systems/x86_64-linux/homelab/proxy/default.nix
@@ -0,0 +1,300 @@
+{
+  config ? throw "not imported as module",
+  lib ? throw "not imported as module",
+  pkgs ? throw "not imported as module",
+  stateVersion ? "25.05",
+  ...
+}:
+let
+  domain = "kike.wtf";
+in
+{
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                   Boot                    ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  boot.loader.grub.enable = true;
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                   Disko                   ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  disko.devices = {
+    disk."disk0" = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          BOOT = {
+            size = "1M";
+            type = "EF02";
+          };
+          NIXOS = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              extraArgs = [
+                "-L"
+                "NIXOS"
+              ];
+            };
+          };
+        };
+      };
+    };
+    nodev."/tmp" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "noexec"
+        "size=1G"
+      ];
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                Environment                ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  environment.systemPackages = with pkgs; [
+    curl
+    openssl
+  ];
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                 Hardware                  ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  hardware.enableAllHardware = true;
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                Inputs: Age                ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  age = {
+    identityPaths = builtins.map (key: key.path) config.services.openssh.hostKeys;
+    secrets = {
+      "acme.env".file = ./.acme/acme.env.age;
+      "homepage.env".file = ./.homepage/homepage.env.age;
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                Networking                 ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  networking = {
+    # --- host
+    hostName = "proxy";
+    hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
+    # --- interfaces
+    usePredictableInterfaceNames = false;
+    interfaces = {
+      # --- physical interfaces
+      "eth0" = {
+        useDHCP = false;
+        ipv4.addresses = [
+          {
+            address = "10.0.10.1";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+    # --- gateway
+    defaultGateway = {
+      interface = "eth0";
+      address = "10.0.10.254";
+    };
+    # --- dns
+    domain = "home";
+    search = [ "home" ];
+    nameservers = [ "10.0.10.254" ];
+    # --- firewall
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        443
+        64022
+      ];
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                  Nixpkgs                  ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  nixpkgs.config.allowUnfree = false;
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                  Security                 ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  security = {
+    # --- acme
+    acme = {
+      acceptTerms = true;
+      certs."${domain}" = {
+        inherit domain;
+        inherit (config.services.caddy) group;
+        dnsPropagationCheck = true;
+        dnsProvider = "cloudflare";
+        dnsResolver = "1.1.1.1:53";
+        email = "acme@${domain}";
+        environmentFile = config.age.secrets."acme.env".path;
+        extraDomainNames = [ "*.${domain}" ];
+        renewInterval = "90d";
+      };
+    };
+    # --- pam
+    pam = {
+      sshAgentAuth.enable = true;
+      services."sudo".sshAgentAuth = true;
+    };
+    # --- sudo
+    sudo-rs = {
+      enable = true;
+      execWheelOnly = true;
+    };
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                 Services                  ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  services = {
+
+    # ┌──────────────────────────────────────┐
+    # │                 Caddy                │
+    # └──────────────────────────────────────┘
+
+    caddy = {
+      enable = true;
+      enableReload = true;
+      logFormat = "level INFO";
+      configFile = ./.caddy/Caddyfile;
+    };
+
+    # ┌──────────────────────────────────────┐
+    # │               Homepage               │
+    # └──────────────────────────────────────┘
+
+    homepage-dashboard =
+      let
+        homepageConfig = builtins.fromJSON (builtins.readFile ./.homepage/homepage.json);
+      in
+      {
+        inherit (homepageConfig) services;
+        enable = true;
+        listenPort = 8082;
+        allowedHosts = "*";
+        environmentFile = config.age.secrets."homepage.env".path;
+        settings = {
+          inherit (homepageConfig) layout;
+          color = "slate";
+          title = "Kike's Homelab";
+          description = "A collection of services running on Kike's Homelab";
+          hideVersion = true;
+          useEqualHeights = true;
+        };
+      };
+
+    # ┌──────────────────────────────────────┐
+    # │                OpenSSH               │
+    # └──────────────────────────────────────┘
+
+    openssh = {
+      enable = true;
+      allowSFTP = true;
+      authorizedKeysInHomedir = false;
+      banner = builtins.readFile ./.ssh/banner.txt;
+      listenAddresses = [
+        {
+          addr = "0.0.0.0";
+          port = 64022;
+        }
+      ];
+      ports = [ ];
+      startWhenNeeded = true;
+      settings = {
+        AllowUsers = lib.attrsets.mapAttrsToList (name: _: name) (
+          lib.attrsets.filterAttrs (_: v: builtins.elem "sshuser" v.extraGroups) config.users.users
+        );
+        AuthorizedPrincipalsFile = "none";
+        ChallengeResponseAuthentication = false;
+        Ciphers = [
+          "chacha20-poly1305@openssh.com"
+          "aes256-gcm@openssh.com"
+          "aes128-gcm@openssh.com"
+          "aes256-ctr"
+          "aes192-ctr"
+          "aes128-ctr"
+        ];
+        ClientAliveInterval = 300;
+        GatewayPorts = "no";
+        IgnoreRhosts = true;
+        KbdInteractiveAuthentication = false;
+        KexAlgorithms = [
+          "curve25519-sha256@libssh.org"
+          "diffie-hellman-group16-sha512"
+          "diffie-hellman-group18-sha512"
+        ];
+        LogLevel = "VERBOSE";
+        LoginGraceTime = "30";
+        Macs = [
+          "hmac-sha2-512-etm@openssh.com"
+          "hmac-sha2-256-etm@openssh.com"
+          "umac-128-etm@openssh.com"
+        ];
+        MaxAuthTries = 3;
+        MaxSessions = 5;
+        MaxStartups = "10:30:100";
+        PasswordAuthentication = false;
+        PermitEmptyPasswords = false;
+        PermitRootLogin = "no";
+        PrintMotd = false;
+        StrictModes = true;
+        UseDns = false;
+        UsePAM = true;
+        X11Forwarding = false;
+      };
+    };
+
+  };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                  System                   ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  system = { inherit stateVersion; };
+
+  # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  # ┃                   Users                   ┃
+  # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  users = {
+    mutableUsers = false;
+    groups = {
+      sshuser = { };
+      users = { };
+    };
+    users.proxy = {
+      isNormalUser = true;
+      description = "Proxy management user";
+      initialPassword = null;
+      home = "/home/users/proxy";
+      uid = 1000;
+      group = "users";
+      useDefaultShell = true;
+      extraGroups = [
+        "wheel"
+        "sshuser"
+      ];
+      openssh.authorizedKeys.keys = lib.strings.splitString "\n" (
+        builtins.readFile ./.ssh/authorized_keys
+      );
+    };
+  };
+}
