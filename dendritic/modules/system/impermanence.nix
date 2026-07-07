d07fce41b86f3273660381c9a3c168868f18d775
Kike Fontán <26680023+CosasDePuma@users.noreply.github.com>
2026-07-07 21:57:42 +0200

    fix(system-impermanence): make libvirt conditional

--- dendritic/modules/system/impermanence.nix
+++ dendritic/modules/system/impermanence.nix
@@ -1,19 +1,24 @@
-{inputs, ...}: {
-  flake.nixosModules.system-impermanence = {
+{
+  inputs,
+  lib,
+  ...
+}: {
+  flake.nixosModules.system-impermanence = {config, ...}: {
     imports = [
       inputs.impermanence.nixosModules.impermanence
     ];
 
     environment.persistence."/nix/persist" = {
       hideMounts = true;
-      directories = [
-        "/var/log"
-        "/var/lib/nixos"
-        "/var/lib/systemd"
-        "/etc/NetworkManager/system-connections"
-        "/etc/ssh"
-        "/var/lib/libvirt"
-      ];
+      directories =
+        [
+          "/var/log"
+          "/var/lib/nixos"
+          "/var/lib/systemd"
+          "/etc/NetworkManager/system-connections"
+          "/etc/ssh"
+        ]
+        ++ lib.optional config.virtualisation.libvirtd.enable "/var/lib/libvirt";
       files = [
         "/etc/machine-id"
       ];
