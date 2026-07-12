f12f9fc5ef4500528a17cdcbafdba659d792be7b
Kike Fontán <26680023+CosasDePuma@users.noreply.github.com>
2026-07-12 16:52:29 +0200

    fix(homelab): set initialPassword to null for user configuration

--- dendritic/hosts/homelab/services/default.nix
+++ dendritic/hosts/homelab/services/default.nix
@@ -41,7 +41,7 @@
         uid = 1000;
         group = "users";
         useDefaultShell = true;
-        initialPassword = "nixos";
+        initialPassword = null;
         extraGroups = [
           "wheel"
           "sshusers"
